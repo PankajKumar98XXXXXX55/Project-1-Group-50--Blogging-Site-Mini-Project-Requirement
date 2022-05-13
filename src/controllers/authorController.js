24ae2ae95d7fad9613eb6bd4f8ab323be33d6c46
Pankajsingh2110 <ddmukharji@gmail.com>
2022-05-13 08:24:47 +0530

    ## Blogging Site Mini Project Requirement

---
+++ src/controllers/authorController.js
@@ -0,0 +1,50 @@
+const authorModel = require("../models/authorModel");
+const jwt = require("jsonwebtoken")
+
+const createAuthor = async (req, res) => {
+  try {
+    if (!req.body.firstName || !req.body.email || !req.body.password)
+      return res.status(400).send("name,email,passwords are a required field");
+    //console.log("name,email,passwords are a required field");
+    const savedData = await authorModel.create(req.body);
+    console.log("Author created Successfully");
+    return res.status(200).send({ msg: savedData });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+};
+
+
+const login = async function(req,res){
+    try{
+        let data = req.body
+        let{email,password} = data
+
+        if(!email)
+        return res.status(404).send({status:false,msg:"email id is missing"})
+        
+        if(!password)
+        return res.status(404).send({status:false,msg:"password is missing"})
+
+        let author = await authorModel.findOne({email:email, password:password})
+        if(!author)
+        return res.status(404).send({status:false,msg:"invalid email or password"})
+
+        let token = jwt.sign({
+            author_Id : author._id.toString()
+        }, "project-one");
+
+        res.setHeader("x-api-key", token);
+        res.status(200).send({ status: true, data: token });
+
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(500).send({error:err.message})
+
+    }
+}
+
+
+module.exports.createAuthor = createAuthor;
+module.exports.login = login
---
+++ src/models/blogModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const authorModel = require("./authorModel")
+const Objectid = mongoose.Schema.Types.ObjectId
+
+const blogSchema = new mongoose.Schema({
+    title : {type : String, required : true},
+    body : {type : String, required : true},
+    authorId : {
+        type : Objectid,
+        ref : "blogAuthor",
+        required : true
+    },
+    tags : [String],
+    category : {
+        type : [String],
+        required : true
+    },
+    subcategory : {
+        type : [String],
+        required : true
+    },
+    published : {
+        type : Boolean,
+        default : true
+    },
+    publishedAt : Date,
+    isDeleted : {
+        default : false,
+        type : Boolean
+    },
+    deletedAt : Date
+}, {timestamps : true})
+
+module.exports= mongoose.model("blog", blogSchema)
